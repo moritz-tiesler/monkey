e21fc659fef4adf4e403776a9209120615ca9055
Moritz Tiesler <moritz.tiesler@t-online.de>
2023-07-25 21:14:18 +0200

    more tokens

--- interpreter/src/lexer/lexer_test.go
+++ interpreter/src/lexer/lexer_test.go
@@ -5,57 +5,32 @@ import (
 	"testing"
 )
 
-type Test struct {
-	expectedType    token.TokenType
-	expectedLiteral string
-}
-
 func TestNextToken(t *testing.T) {
-	input := `+=(){},;`
-
-	tests := []Test{
-		{token.PLUS, "+"},
-		{token.ASSIGN, "="},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
-	l := New(input)
-
-	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Literal)
-		}
-	}
-}
-
-func Run(input string, expected []Test) {
-
-}
-
-func TestNextToken2(t *testing.T) {
 	input := `let five = 5;
-		let ten = 10;
-		let add = fn(x, y) {
-			x + y;
-		};
-		let result = add(five, ten);
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
 `
 
-	tests := []Test{
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
 		{token.LET, "let"},
 		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
@@ -92,7 +67,44 @@ func TestNextToken2(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		// {token.IF, "if"},
+		// {token.LPAREN, "("},
+		// {token.INT, "5"},
+		// {token.LT, "<"},
+		// {token.INT, "10"},
+		// {token.RPAREN, ")"},
+		// {token.LBRACE, "{"},
+		// {token.RETURN, "return"},
+		// {token.TRUE, "true"},
+		// {token.SEMICOLON, ";"},
+		// {token.RBRACE, "}"},
+		// {token.ELSE, "else"},
+		// {token.LBRACE, "{"},
+		// {token.RETURN, "return"},
+		// {token.FALSE, "false"},
+		// {token.SEMICOLON, ";"},
+		// {token.RBRACE, "}"},
+		// {token.INT, "10"},
+		// {token.EQ, "=="},
+		// {token.INT, "10"},
+		// {token.SEMICOLON, ";"},
+		// {token.INT, "10"},
+		// {token.NOT_EQ, "!="},
+		// {token.INT, "9"},
+		// {token.SEMICOLON, ";"},
+		// {token.EOF, ""},
 	}
 
 	l := New(input)
@@ -107,7 +119,7 @@ func TestNextToken2(t *testing.T) {
 
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Literal)
+				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
--- interpreter/src/lexer/token/token.go
+++ interpreter/src/lexer/token/token.go
@@ -14,8 +14,15 @@ const (
 	IDENT = "IDENT"
 	INT   = "INT"
 
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	COMMA     = ","
 	SEMICOLON = ";"
