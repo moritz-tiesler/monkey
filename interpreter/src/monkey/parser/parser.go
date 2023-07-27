a62805bafdc6f22010506d778fc004c2ba203e16
Moritz Tiesler <moritz.tiesler@t-online.de>
2023-07-27 22:27:51 +0200

    init ast and parser

---
+++ interpreter/src/monkey/parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.NextToken()
+	p.NextToken()
+
+	return p
+}
+
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
