063e67b281ce28c5ca2714b0323a1b80456e4068
Moritz Tiesler <moritz.tiesler@t-online.de>
2023-08-03 20:41:45 +0200

    parse return statements

--- interpreter/src/monkey/parser/parser.go
+++ interpreter/src/monkey/parser/parser.go
@@ -51,6 +51,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -70,6 +72,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: We are skipping the expression until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.NextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.NextToken()
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.NextToken()
 	}
