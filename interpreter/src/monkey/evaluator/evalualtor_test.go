7aebc0e977bfa0eff18f333c6e96371764466427
Moritz Tiesler <moritz.tiesler@t-online.de>
2023-09-27 19:39:30 +0200

    page 159: string concatenation

--- interpreter/src/monkey/evaluator/evalualtor_test.go
+++ interpreter/src/monkey/evaluator/evalualtor_test.go
@@ -244,6 +244,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"Error at line 1 col 1, identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -319,6 +323,20 @@ func TestStringLiteral(t *testing.T) {
 	if !ok {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
