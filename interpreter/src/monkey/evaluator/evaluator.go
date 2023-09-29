5b35578159aadaa56b05b3c437a3ed47018fadcd
Moritz Tiesler <moritz.tiesler@t-online.de>
2023-09-29 20:19:26 +0200

    page 174: parse and eval array literals

--- interpreter/src/monkey/evaluator/evaluator.go
+++ interpreter/src/monkey/evaluator/evaluator.go
@@ -79,7 +79,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		params := node.Parameters
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
-
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
 	case *ast.CallExpression:
 		function := Eval(node.Function, env)
 		if isError(function) {
--- interpreter/src/monkey/parser/parser.go
+++ interpreter/src/monkey/parser/parser.go
@@ -47,6 +47,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
@@ -365,6 +366,16 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
 	return array
 }
 
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+	return exp
+}
+
 func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 	list := []ast.Expression{}
 
@@ -433,6 +444,7 @@ const (
 	PRODUCT
 	PREFIX
 	CALL
+	INDEX // array[index]
 )
 
 var precedences = map[token.TokenType]int{
@@ -445,6 +457,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 func (p *Parser) Errors() []string {
