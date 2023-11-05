63fd16fff674ddcdbb6dc0ca9fb725bf7bbbbed4
Moritz Tiesler <moritz.tiesler@t-online.de>
2023-11-05 15:25:32 +0100

    page 194: eval hashmaps

--- interpreter/src/monkey/evaluator/evaluator.go
+++ interpreter/src/monkey/evaluator/evaluator.go
@@ -107,6 +107,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args)
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 
 	return nil
@@ -303,6 +305,32 @@ func evalIdentifier(
 	return newError(
 		fmt.Sprintf("Error at line %d col %d, identifier not found: %s",
 			node.Token.Line, node.Token.Col, node.Value))
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
 
 func isTruthy(obj object.Object) bool {
