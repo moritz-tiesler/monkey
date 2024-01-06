6e5a07bfe4055c6be5e8ebff4045466e7694e772
Moritz Tiesler <moritz.tiesler@t-online.de>
2024-01-06 15:58:00 +0100

    page 163: compile function literals

--- interpreter/src/monkey/code/code.go
+++ interpreter/src/monkey/code/code.go
@@ -57,6 +57,9 @@ const (
 	OpArray
 	OpHash
 	OpIndex
+	OpCall
+	OpReturnValue
+	OpReturn
 )
 
 type Definition struct {
@@ -86,6 +89,9 @@ var definitions = map[Opcode]*Definition{
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
 	OpIndex:         {"OpIndex", []int{}},
+	OpCall:          {"OpCall", []int{}},
+	OpReturnValue:   {"OpReturnValue", []int{}},
+	OpReturn:        {"OpReturn", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
--- interpreter/src/monkey/object/object.go
+++ interpreter/src/monkey/object/object.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"hash/fnv"
 	"monkey/ast"
+	"monkey/code"
 	"strings"
 )
 
@@ -19,10 +20,11 @@ const (
 
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 
-	FUNCTION_OBJ = "FUNCTION"
-	STRING_OBJ   = "STRING"
-	BUILTIN_OBJ  = "BUILTIN"
-	ARRAY_OBJ    = "ARRAY"
+	FUNCTION_OBJ          = "FUNCTION"
+	COMPILED_FUNCTION_OBJ = "COMPILED_FUNCTION_OBJ"
+	STRING_OBJ            = "STRING"
+	BUILTIN_OBJ           = "BUILTIN"
+	ARRAY_OBJ             = "ARRAY"
 
 	HASH_OBJ = "HASH"
 )
@@ -95,6 +97,15 @@ func (f *Function) Inspect() string {
 	out.WriteString("\n}")
 
 	return out.String()
+}
+
+type CompiledFunction struct {
+	Instructions code.Instructions
+}
+
+func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
+func (cf *CompiledFunction) Inspect() string {
+	return fmt.Sprintf("CompiledFunction[%p]", cf)
 }
 
 type BuiltinFunction func(args ...Object) Object
