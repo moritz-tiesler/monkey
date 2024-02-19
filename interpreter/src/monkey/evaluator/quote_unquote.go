2ec66cd1e1539f96507e91036e0bb0e73973cd45
Moritz Tiesler <moritz.tiesler@t-online.de>
2024-02-19 20:39:11 +0100

    chapter 5.4

--- interpreter/src/monkey/evaluator/quote_unquote.go
+++ interpreter/src/monkey/evaluator/quote_unquote.go
@@ -42,12 +42,25 @@ func isUnquotedCall(node ast.Node) bool {
 
 func convertObjectToASTNode(obj object.Object) ast.Node {
 	switch obj := obj.(type) {
+
 	case *object.Integer:
 		t := token.Token{
 			Type:    token.INT,
 			Literal: fmt.Sprintf("%d", obj.Value),
 		}
 		return &ast.IntegerLiteral{Token: t, Value: obj.Value}
+
+	case *object.Boolean:
+		var t token.Token
+		if obj.Value {
+			t = token.Token{Type: token.TRUE, Literal: "true"}
+		} else {
+			t = token.Token{Type: token.FALSE, Literal: "false"}
+		}
+		return &ast.Boolean{Token: t, Value: obj.Value}
+
+	case *object.Quote:
+		return obj.Node
 
 	default:
 		return nil
--- interpreter/src/monkey/evaluator/quote_unquote_test.go
+++ interpreter/src/monkey/evaluator/quote_unquote_test.go
@@ -79,6 +79,23 @@ func TestQuoteUnquote(t *testing.T) {
             quote(unquote(foobar))`,
 			`8`,
 		},
+		{
+			`quote(unquote(true))`,
+			`true`,
+		},
+		{
+			`quote(unquote(true == false))`,
+			`false`,
+		},
+		{
+			`quote(unquote(quote(4 + 4)))`,
+			`(4 + 4)`,
+		},
+		{
+			`let quotedInfixExpression = quote(4 + 4);
+            quote(unquote(4 + 4) + unquote(quotedInfixExpression))`,
+			`(8 + (4 + 4))`,
+		},
 	}
 
 	for _, tt := range tests {
