c066243d0b56c4fb7d7639eea9cf6febd452bbd9
Moritz Tiesler <moritz.tiesler@t-online.de>
2024-02-19 14:28:24 +0100

    flag for interpreted or compiled mode

--- interpreter/src/monkey/main.go
+++ interpreter/src/monkey/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"monkey/repl"
 	"os"
@@ -8,11 +9,22 @@ import (
 )
 
 func main() {
+	// interpretPtr := flag.Bool("i", true, "runs monkey with interpreter")
+	compilePtr := flag.Bool("c", false, "runs monkey with compiler")
+	flag.Parse()
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Hello %s! This is the monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
-	repl.StartVM(os.Stdin, os.Stdout)
+
+	switch *compilePtr {
+	case true:
+		fmt.Printf("Compiling\n")
+		repl.StartVM(os.Stdin, os.Stdout)
+	default:
+		fmt.Printf("Interpreting\n")
+		repl.StartInterpreter(os.Stdin, os.Stdout)
+	}
 }
--- interpreter/src/monkey/repl/repl.go
+++ interpreter/src/monkey/repl/repl.go
@@ -175,3 +175,32 @@ func StartStream(in chan string, out chan string) {
 		out <- (stackTop.Inspect() + "\n")
 	}
 }
+
+func StartInterpreter(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
