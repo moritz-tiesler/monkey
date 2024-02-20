f0375d32895c4fc4004c42f5ccf99873ce373586
Moritz Tiesler <moritz.tiesler@t-online.de>
2024-02-20 01:56:31 +0100

    check argument number in evaluator

--- interpreter/src/monkey/evaluator/evaluator.go
+++ interpreter/src/monkey/evaluator/evaluator.go
@@ -399,6 +399,9 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 	switch fn := fn.(type) {
 
 	case *object.Function:
+		if len(args) != len(fn.Parameters) {
+			return newError("wrong number of arguments: want=%d, got=%d", len(fn.Parameters), len(args))
+		}
 		extendedEnv := extendFunctionEnv(fn, args)
 		evaluated := Eval(fn.Body, extendedEnv)
 		return unwrapReturnValue(evaluated)
