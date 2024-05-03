312c45e3b551b0e4fde07d88df241cd194baa725
Moritz Tiesler <moritz.tiesler@t-online.de>
2024-05-03 19:20:36 +0200

    tight loop and CurrentOpIsJump

--- vm/vm.go
+++ vm/vm.go
@@ -482,7 +482,6 @@ func (vm *VM) SourceLocationInFrame(f *Frame) compiler.LocationData {
 	if vm.CurrentInstructionIsPop() {
 		return vm.mustFindLocation(f, f.Ip-1)
 	} else {
-		vm.CurrentInstructionIsPop()
 		return vm.mustFindLocation(f, f.Ip)
 	}
 }
@@ -923,4 +922,231 @@ func (vm *VM) CurrentInstructionIsPop() bool {
 	}
 	ins := f.Instructions()[ip]
 	return ins == byte(code.OpPop)
+}
+
+func (vm *VM) CurrentInstructionIsJump() bool {
+
+	f := vm.CurrentFrame()
+	ip := f.Ip - 1
+	if ip < 0 {
+		return false
+	}
+	ins := f.Instructions()[ip]
+	return ins == byte(code.OpJumpNotTruthy) || ins == byte(code.OpJump)
+}
+
+func (vm *VM) RunTight() error {
+	var ip int
+	var ins code.Instructions
+	var op code.Opcode
+
+	for vm.CurrentFrame().Ip < len(vm.CurrentFrame().Instructions())-1 {
+		vm.CurrentFrame().Ip++
+
+		ip = vm.CurrentFrame().Ip
+		ins = vm.CurrentFrame().Instructions()
+		op = code.Opcode(ins[ip])
+
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(ins[ip+1:])
+			vm.CurrentFrame().Ip += 2
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+
+		case code.OpSetGlobal:
+			globalIndex := code.ReadUint16(ins[ip+1:])
+			vm.CurrentFrame().Ip += 2
+			vm.globals[globalIndex] = vm.pop()
+
+		case code.OpGetGlobal:
+			globalIndex := code.ReadUint16(ins[ip+1:])
+			vm.CurrentFrame().Ip += 2
+			err := vm.push(vm.globals[globalIndex])
+			if err != nil {
+				return err
+			}
+
+		case code.OpAdd, code.OpSub, code.OpMul, code.OpDiv:
+			err := vm.executeBinaryOperation(op)
+			if err != nil {
+				return err
+			}
+
+		case code.OpEqual, code.OpNotEqual, code.OpGreaterThan:
+			err := vm.executeComparison(op)
+			if err != nil {
+				return err
+			}
+
+		case code.OpBang:
+			err := vm.executeBangOperator()
+			if err != nil {
+				return err
+			}
+
+		case code.OpMinus:
+			err := vm.executeMinusOperator()
+			if err != nil {
+				return err
+			}
+
+		case code.OpTrue:
+			err := vm.push(True)
+			if err != nil {
+				return err
+			}
+
+		case code.OpFalse:
+			err := vm.push(False)
+			if err != nil {
+				return err
+			}
+
+		case code.OpPop:
+			vm.pop()
+
+		case code.OpJump:
+			pos := int(code.ReadUint16(ins[ip+1:]))
+			vm.CurrentFrame().Ip = pos - 1
+
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(ins[ip+1:]))
+			vm.CurrentFrame().Ip += 2
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				vm.CurrentFrame().Ip = pos - 1
+			}
+
+		case code.OpNull:
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
+		case code.OpArray:
+			numElements := int(code.ReadUint16(ins[ip+1:]))
+			vm.CurrentFrame().Ip += 2
+			array := vm.buildArray(vm.sp-numElements, vm.sp)
+			vm.sp -= numElements
+
+			err := vm.push(array)
+			if err != nil {
+				return err
+			}
+
+		case code.OpHash:
+			numElements := int(code.ReadUint16(ins[ip+1:]))
+			vm.CurrentFrame().Ip += 2
+
+			hash, err := vm.buildHash(vm.sp-numElements, vm.sp)
+			if err != nil {
+				return err
+			}
+			vm.sp = vm.sp - numElements
+
+			err = vm.push(hash)
+			if err != nil {
+				return err
+			}
+		case code.OpIndex:
+			index := vm.pop()
+			left := vm.pop()
+
+			err := vm.executeIndexExpression(left, index)
+			if err != nil {
+				return err
+			}
+
+		case code.OpSetLocal:
+			localIndex := code.ReadUint8(ins[ip+1:])
+			vm.CurrentFrame().Ip += 1
+
+			frame := vm.CurrentFrame()
+
+			vm.stack[frame.basePointer+int(localIndex)] = vm.pop()
+
+		case code.OpGetLocal:
+			localIndex := code.ReadUint8(ins[ip+1:])
+			vm.CurrentFrame().Ip += 1
+
+			frame := vm.CurrentFrame()
+
+			err := vm.push(vm.stack[frame.basePointer+int(localIndex)])
+			if err != nil {
+				return err
+			}
+
+		case code.OpCall:
+			numArgs := code.ReadUint8(ins[ip+1:])
+			vm.CurrentFrame().Ip += 1
+
+			err := vm.executeCall(int(numArgs))
+			if err != nil {
+				return err
+			}
+
+		case code.OpReturnValue:
+			returnValue := vm.pop()
+
+			frame := vm.popFrame()
+			vm.sp = frame.basePointer - 1
+
+			err := vm.push(returnValue)
+			if err != nil {
+				return err
+			}
+
+		case code.OpReturn:
+			frame := vm.popFrame()
+			vm.sp = frame.basePointer - 1
+
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
+		case code.OpGetBuiltin:
+			builinIndex := code.ReadUint8(ins[ip+1:])
+			vm.CurrentFrame().Ip += 1
+
+			definition := object.Builtins[builinIndex]
+
+			err := vm.push(definition.Builtin)
+			if err != nil {
+				return err
+			}
+
+		case code.OpClosure:
+			constIndex := code.ReadUint16(ins[ip+1:])
+			numFree := code.ReadUint8(ins[ip+3:])
+			vm.CurrentFrame().Ip += 3
+
+			err := vm.pushClosure(int(constIndex), int(numFree))
+			if err != nil {
+				return err
+			}
+
+		case code.OpGetFree:
+			freeIndex := code.ReadUint8(ins[ip+1:])
+			vm.CurrentFrame().Ip += 1
+
+			currentClosure := vm.CurrentFrame().cl
+
+			err := vm.push(currentClosure.Free[freeIndex])
+			if err != nil {
+				return err
+			}
+		case code.OpCurrentClosure:
+			currentClosure := vm.CurrentFrame().cl
+			err := vm.push(currentClosure)
+			if err != nil {
+				return err
+			}
+
+		}
+
+	}
+
+	return nil
 }
